e4061c05421584a33d4ff8efa2679d3abab8e3a3
KVSSetty <kvssetty@gmail.com>
2018-08-20 14:37:54 +0530

    sourceedge scripts floder

---
+++ sourceedge/preprocessing/imagetoarraypreprocessor.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 18 16:15:43 2018
+
+@author: kvs
+"""
+
+# import the necessary packages
+from keras.preprocessing.image import img_to_array
+
+class ImageToArrayPreprocessor:
+	def __init__(self, dataFormat=None):
+		# store the image data format
+		self.dataFormat = dataFormat
+
+	def preprocess(self, image):
+		# apply the Keras utility function that correctly rearranges
+		# the dimensions of the image
+		return img_to_array(image, data_format=self.dataFormat)
